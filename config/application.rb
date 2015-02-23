e57390883652d540d167c44c96b38eb9a07ef3ab
Ioanna Arka <ioannaarka@gmail.com>
2015-02-23 11:42:59 +0100

    STILL trying to fix famfamfam

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require File.expand_path('../boot', __FILE__)
-config.serve_static_assets = true
 
 require 'rails/all'
 
@@ -9,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module Ioanna
   class Application < Rails::Application
+    
+    config.serve_static_assets = true
+  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
